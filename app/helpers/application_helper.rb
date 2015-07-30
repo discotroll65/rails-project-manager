5dcc683814aacd3a119b511e119e5a981ee124c5
Garrett Simpson <elgaricimo@gmail.com>
2015-07-30 11:55:51 -0700

    Extract out 'form for' form in _form partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def csrf_tag
+    html = <<-HTML
+    <input
+      type="hidden"
+      name="authenticity_token"
+      value="#{form_authenticity_token}">
+    HTML
+
+    html.html_safe
+  end
 end
